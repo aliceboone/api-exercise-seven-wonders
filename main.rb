5bbd94f856bc4011dd45e0c8fb68633becd7d65a
Alice Boone <alicegbonel@gmail.com>
2020-10-05 19:18:26 -0700

    Update to the seven_wonders.rb

---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative 'seven_wonders'
+
+location = get_location("Great Pyramid of Giza")
+puts location
+
+find = find_seven_wonders
+puts find
+
--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,11 +1,29 @@
 require 'httparty'
-require "awesome_print"
+require 'awesome_print'
+require 'pry'
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-LOCATION_IQ_KEY = "YOUR API TOKEN"
+BASE_URL = "https://us1.locationiq.com/v1/search.php"
+KEY = "pk.94740926b6cd4c11828d7ac78f8f23cd"
+class SearchError < StandardError; end
 
 def get_location(search_term)
+  query = {
+      q: search_term,
+      key: KEY,
+      format: "json",
+  }
+  response = HTTParty.get(BASE_URL, query: query)
 
+  unless response.code == 200
+    raise SearchError, "Cannot find #{search_term}"
+  end
+
+  return {
+      search_term => {
+          lat: response.first["lat"],
+          lon: response.first["lon"]
+      },
+  }
 end
 
 def find_seven_wonders
@@ -21,9 +39,3 @@ def find_seven_wonders
 
   return seven_wonders_locations
 end
-
-
-# Use awesome_print because it can format the output nicely
-ap find_seven_wonders
-# Expecting something like:
-# [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
